2b60daf0ed1930788cf5485ef283a3a72148ccc2
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-07-22 20:07:41 -0500

    initial setup. local host listening

---
+++ server/public/scripts/client.js
@@ -0,0 +1,2 @@
+console.log('client js sourced');
+var myApp = angular.module('myApp', []);
---
+++ server/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var saleRoute = require('./routes/sale.route.js');
+var rentRoute = require('./routes/rent.route.js');
+var mongoose = require('mongoose');
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.json()); // needed for angular requests
+
+/** ---------- EXPRESS ROUTES ---------- **/
+// not active routes yet
+// app.use('/rent', rentRoute);
+// app.use('/sale', saleRoute);
+
+/** ---------- GET INDEX HTML ---------- **/
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+});
+
+/** -------- MONGOOSE CONNECTION --------**/
+var databaseUrl = 'mongodb://localhost:27017/antares';
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', function() {
+  console.log('mongoose connected to : ', databaseUrl);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.log('mongoose connection error: ', err);
+});
+
+
+
+/** ---------- START SERVER ---------- **/
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
+});
